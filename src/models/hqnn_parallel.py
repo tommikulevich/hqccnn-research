1d5ed8e679c4e32bb5b3968c91f34661ef44504f
Tomash Mikulevich <tommikulevich@gmail.com>
2025-05-07 21:47:07 +0200

    Adapt HQNN-Parallel to the version discussed in the original article. Increase readability of the model.

--- src/models/hqnn_parallel.py
+++ src/models/hqnn_parallel.py
@@ -14,11 +14,11 @@ def get_quantum_circuit(wires: int) -> Callable:
     """Wrap a PennyLane device into a torch-compatible QNode."""
     device = qml.device("default.qubit", wires=wires) if dev is None else dev
 
-    @qml.qnode(device, interface='torch')
+    @qml.qnode(device, interface='torch', diff_method="parameter-shift")
     def quantum_circuit(inputs, weights):
-        qml.AngleEmbedding(inputs, wires=list(range(wires)))
-        qml.BasicEntanglerLayers(weights, wires=list(range(wires)))
-        return [qml.expval(qml.PauliZ(wires=w)) for w in range(wires)]
+        qml.AngleEmbedding(inputs, rotation='X', wires=list(range(wires)))
+        qml.StronglyEntanglingLayers(weights, wires=list(range(wires)))
+        return [qml.expval(qml.PauliY(wires=w)) for w in range(wires)]
 
     return quantum_circuit
 
@@ -26,63 +26,83 @@ def get_quantum_circuit(wires: int) -> Callable:
 class HQNN_Parallel(nn.Module):
     """HQNN_Parallel model combining classical CNN and quantum layers."""
     def __init__(self, in_channels: int, num_classes: int,
-                 conv_channels: List[int], kernel_sizes: List[int],
-                 pool_sizes: List[int], fc_sizes: List[int],
-                 num_qubits: int, num_qlayers: int) -> None:
+                 conv_channels: List[int], conv_kernels: List[int],
+                 conv_strides: List[int], conv_paddings: List[int],
+                 pool_sizes: List[int], num_qubits: int,
+                 num_qlayers: int, num_qrepetitions: int) -> None:
         super().__init__()
 
+        self.num_qubits = num_qubits
+        self.num_qlayers = num_qlayers
+        self.num_q_features = num_qlayers * num_qubits
+
         # Classical convolutional and linear layers
-        self.conv1 = nn.Conv2d(in_channels, conv_channels[0],
-                               kernel_sizes[0], padding=kernel_sizes[0] // 2)
-        self.bn1 = nn.BatchNorm2d(conv_channels[0])
-        self.relu1 = nn.ReLU()
+        self.conv_block1 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=in_channels,
+                out_channels=conv_channels[0],
+                kernel_size=conv_kernels[0],
+                stride=conv_strides[0],
+                padding=conv_paddings[0]
+            ),
+            nn.BatchNorm2d(num_features=conv_channels[0]),
+            nn.ReLU()
+        )
         self.pool1 = nn.MaxPool2d(kernel_size=pool_sizes[0])
 
-        self.conv2 = nn.Conv2d(conv_channels[0], conv_channels[1],
-                               kernel_sizes[1], padding=kernel_sizes[1] // 2)
-        self.bn2 = nn.BatchNorm2d(conv_channels[1])
-        self.relu2 = nn.ReLU()
+        self.conv_block2 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=conv_channels[0],
+                out_channels=conv_channels[1],
+                kernel_size=conv_kernels[1],
+                stride=conv_strides[1],
+                padding=conv_paddings[1]
+            ),
+            nn.BatchNorm2d(num_features=conv_channels[1]),
+            nn.ReLU()
+        )
         self.pool2 = nn.MaxPool2d(kernel_size=pool_sizes[1])
 
         self.flatten = nn.Flatten(start_dim=1)
 
-        self.fc1 = nn.LazyLinear(fc_sizes[0])  # To not calculate in_features
-        self.relu3 = nn.ReLU()
-        self.fc2 = nn.Linear(fc_sizes[0], fc_sizes[1])
-        self.relu4 = nn.ReLU()
+        self.fc_block1 = nn.Sequential(
+            nn.LazyLinear(out_features=self.num_q_features),
+            nn.BatchNorm1d(num_features=self.num_q_features),
+            nn.ReLU()
+        )
 
         # Quantum layers
-        self.num_qubits = num_qubits
-        weight_shapes = {"weights": (num_qlayers, num_qubits)}
+        weight_shapes = {"weights": qml.StronglyEntanglingLayers.shape(
+            n_layers=num_qrepetitions, n_wires=num_qubits)}
         circuit = get_quantum_circuit(num_qubits)
 
-        assert fc_sizes[1] % num_qubits == 0
-        self.qnetlayers = nn.ModuleList([
+        self.qlayers = nn.ModuleList([
             TorchLayer(circuit, weight_shapes)
-            for _ in range(fc_sizes[1] // num_qubits)
+            for _ in range(num_qlayers)
         ])
 
         # Classical linear layer
-        self.fc3 = nn.Linear(fc_sizes[1], num_classes)
+        self.fc_block2 = nn.Sequential(
+            nn.LazyLinear(out_features=num_classes),
+            nn.BatchNorm1d(num_features=num_classes),
+            nn.ReLU()
+        )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        x = self.relu1(self.bn1(self.conv1(x)))
+        x = self.conv_block1(x)
         x = self.pool1(x)
-        x = self.relu2(self.bn2(self.conv2(x)))
+
+        x = self.conv_block2(x)
         x = self.pool2(x)
 
         x = self.flatten(x)
 
-        x = self.relu3(self.fc1(x))
-        x = self.relu4(self.fc2(x))
+        x = self.fc_block1(x)
 
-        x_chunks = torch.split(x, self.num_qubits, dim=1)
-        q_outs = [
-            layer(chunk)
-            for layer, chunk in zip(self.qnetlayers, x_chunks)
-        ]
-        x = torch.cat(q_outs, dim=1)
+        x_c = x.chunk(self.num_qlayers, dim=1)
+        q = [layer(c) for layer, c in zip(self.qlayers, x_c)]
+        x = torch.cat(q, dim=1)
 
-        x = self.fc3(x)
+        x = self.fc_block2(x)
 
         return x
