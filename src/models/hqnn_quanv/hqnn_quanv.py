faf5731529098424014b2432c0223e4956196071
Tomash Mikulevich <tommikulevich@gmail.com>
2025-07-23 00:27:35 +0200

    Adapt HQNN_Quanv to use another ansatzs.

--- src/models/hqnn_quanv/hqnn_quanv.py
+++ src/models/hqnn_quanv/hqnn_quanv.py
@@ -1,5 +1,6 @@
 """Hybrid QNN Quanvolutional model."""
 import math
+from enum import Enum
 from typing import List, Tuple
 
 import torch
@@ -8,20 +9,45 @@ import torch.nn.functional as F
 import pennylane as qml
 from pennylane.qnn import TorchLayer
 
+try:
+    from utils.seeds import get_seed
+except ImportError:
+    print("Warning: problem with importing utils.seeds. \
+        Set seed to 42 in RandomLayer (if it is used)")
+
+    def get_seed() -> int:
+        return 42
+
+
+# TODO: make it more universal (every type has many different params)
+class AnsatzType(str, Enum):
+    SEL = "sel"     # StronglyEntanglingLayers
+    BEL = "bel"     # BasicEntanglerLayers
+    RL = "rl"       # RandomLayers
+
 
 class Quanv(nn.Module):
     def __init__(self, in_channels: int, kernel_size: int,
                  stride: int, padding: int,
+                 ansatz_type: AnsatzType,
                  num_qubits: int, num_qreps: int,
                  device_name: str = "default.qubit",
                  diff_method: str = "best") -> None:
         super().__init__()
+
+        # Some way to apply string instead of enum
+        if isinstance(ansatz_type, str):
+            try:
+                ansatz_type = AnsatzType[ansatz_type.upper()]
+            except KeyError:
+                raise ValueError(f"Unknown ansatz type: {ansatz_type}")
 
         self.in_channels = in_channels
         self.k = kernel_size
         self.s = stride
         self.p = padding
 
+        self.ansatz_type = ansatz_type
         self.num_qubits = num_qubits
         self.num_qreps = num_qreps
 
@@ -35,21 +61,54 @@ class Quanv(nn.Module):
                                interface="torch",
                                diff_method=diff_method)
 
-        weight_shapes = {
-            "weights": qml.StronglyEntanglingLayers.shape(
-                n_layers=self.num_qreps, n_wires=self.num_qubits
-            )
-        }
+        if ansatz_type is AnsatzType.SEL:
+            weight_shapes = {
+                "weights": qml.StronglyEntanglingLayers.shape(
+                    n_layers=num_qreps, n_wires=num_qubits
+                )
+            }
+        elif ansatz_type is AnsatzType.BEL:
+            weight_shapes = {
+                "weights": qml.BasicEntanglerLayers.shape(
+                    n_layers=num_qreps, n_wires=num_qubits
+                )
+            }
+        elif ansatz_type is AnsatzType.RL:
+            weight_shapes = {
+                "weights": qml.RandomLayers.shape(
+                    n_layers=num_qreps, n_rotations=num_qubits
+                )
+            }
+        else:
+            raise RuntimeError(f"Unknown ansatz type: {ansatz_type}")
 
         self.qlayer = TorchLayer(self.qnode, weight_shapes)
 
     def _circuit(self, inputs: torch.Tensor, weights: torch.Tensor) \
             -> List[torch.Tensor]:
-        qml.AngleEmbedding(inputs, rotation="Y",
+        # Encoding
+        qml.AngleEmbedding(inputs, rotation="X",
                            wires=list(range(self.num_qubits)))
-        qml.StronglyEntanglingLayers(weights,
-                                     wires=list(range(self.num_qubits)))
-        return [qml.expval(qml.PauliZ(wires=i))
+
+        # Ansatz
+        if self.ansatz_type is AnsatzType.SEL:
+            qml.StronglyEntanglingLayers(weights,
+                                         wires=list(range(self.num_qubits)),
+                                         imprimitive=qml.CNOT)
+        elif self.ansatz_type is AnsatzType.BEL:
+            qml.BasicEntanglerLayers(weights,
+                                     wires=list(range(self.num_qubits)),
+                                     rotation=qml.RZ)
+        elif self.ansatz_type is AnsatzType.RL:
+            qml.RandomLayers(weights,
+                             wires=list(range(self.num_qubits)),
+                             imprimitive=qml.CNOT,
+                             seed=get_seed())
+        else:
+            raise RuntimeError(f"Undefined AnsatzType: {self.ansatz_type}")
+
+        # Measure
+        return [qml.expval(qml.PauliY(wires=i))
                 for i in range(self.num_qubits)]
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
@@ -75,8 +134,6 @@ class Quanv(nn.Module):
         wo = patches.size(WIDTH_DIM)
 
         flat = patches.contiguous().view(-1, self.n_inputs)
-        flat = flat.tanh()
-        flat = flat * (math.pi / 2)  # [-pi/2; pi/2]
 
         qout = self.qlayer(flat)                       # [B*Ho*Wo, qubits]
         qm = qout.view(b, ho, wo, self.num_qubits)     # [B, Ho, Wo, qubits]
@@ -89,12 +146,24 @@ class Quanv(nn.Module):
         return qm.permute(OUT_B, OUT_Q, OUT_H, OUT_W)  # [B, qubits, Ho, Wo]
 
 
+class TanhScale(nn.Module):
+    """tanh(x) * scale"""
+    def __init__(self, scale: float = 1) -> None:
+        super().__init__()
+        self.scale = scale
+        self.tanh = nn.Tanh()
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.tanh(x) * self.scale
+
+
 class HQNN_Quanv(nn.Module):
     """HQNN_Quanv model combining quanvolutional layers and classical."""
     def __init__(self, in_channels: int, num_classes: int,
                  input_size: Tuple[int, int, int],
                  quanv_kernels: List[int], quanv_strides: List[int],
                  quanv_paddings: List[int], pool_sizes: List[int],
+                 ansatz_type: AnsatzType,
                  num_qreps: int, qdevice: str, qdiff_method: str) -> None:
         super().__init__()
 
@@ -106,6 +175,7 @@ class HQNN_Quanv(nn.Module):
         assert len(pool_sizes) == n_quanv, \
             "pool_sizes must have the same length as quanv_kernels"
 
+        self.tanh_scale = TanhScale(scale=math.pi/2)
         self.quanv_blocks = nn.ModuleList()
         _, h, w = input_size
         prev_ch = in_channels
@@ -118,7 +188,7 @@ class HQNN_Quanv(nn.Module):
 
             block = nn.Sequential(
                 Quanv(in_channels=prev_ch, num_qubits=out_ch, kernel_size=k,
-                      stride=s, padding=p,
+                      stride=s, padding=p, ansatz_type=ansatz_type,
                       num_qreps=num_qreps, device_name=qdevice,
                       diff_method=qdiff_method),
                 nn.BatchNorm2d(out_ch),
@@ -143,6 +213,7 @@ class HQNN_Quanv(nn.Module):
                             out_features=num_classes)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.tanh_scale(x)
         for quanv_block in self.quanv_blocks:
             x = quanv_block(x)
 
